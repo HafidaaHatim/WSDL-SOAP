1166efe4a3d0ebdaa7a00578d8ba7b6a66daec37
HafidaaHatim <130146750+HafidaaHatim@users.noreply.github.com>
2023-05-31 19:59:35 +0100

    Add files via upload

---
+++ Client SOAP Dot Net.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ClientDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.BanqueServiceClient stub =
+
+            new ServiceReference1.BanqueServiceClient();
+
+            Console.WriteLine("-------- Conversion ---------");
+            Console.WriteLine(stub.C(34));
+            Console.WriteLine("-------- Consulter un Compte ---------");
+            ServiceReference1.compte cp = stub.GetCompte(2);
+            Console.WriteLine("Solde=" + cp.solde);
+            Console.WriteLine("-------- Liste des comptes ---------");
+            ServiceReference1.compte[] cptes = stub.ListComptes();
+            for (int i = 0; i < cptes.Length; i++)
+            {
+                Console.WriteLine(cptes[i].code + "-----" + cptes[i].solde);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
